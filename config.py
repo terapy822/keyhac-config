a12a48754ec13383247ff66a20252aa0a5f851d5
NAOYUKI TERASHITA <naoyuki.terashita.sk@hitachi.com>
2019-03-13 14:40:42 +0900

    initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+﻿import sys
+import os
+import datetime
+
+import pyauto
+from keyhac import *
+
+
+def configure(keymap):
+    keymap_global = keymap.defineWindowKeymap()
+    keymap_global[ "LAlt-A" ] = "Home"
+    keymap_global[ "LAlt-E" ] = "End"
+    keymap_global[ "LAlt-F" ] = "Right"
+    keymap_global[ "LAlt-B" ] = "Left"
+    keymap_global[ "LAlt-N" ] = "Down"
+    keymap_global[ "LAlt-P" ] = "Up"
+    keymap_global[ "LAlt-D" ] = "Delete"
+    keymap_global[ "LAlt-H" ] = "Back"
+
