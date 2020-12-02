a178d20227773b56a26d72d5ef0ac267fb0ae8c4
nayopu <terapy822@gmail.com>
2020-12-02 22:52:31 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-﻿import sys
+import sys
 import os
 import datetime
 
@@ -6,14 +6,19 @@ import pyauto
 from keyhac import *
 
 def configure(keymap):
+    keymap.replaceKey("RCtrl", 235)
+    keymap.defineModifier( 235, "User0" )
+
+    # global
     keymap_global = keymap.defineWindowKeymap()
-    keymap_global[ "LAlt-A" ] = "Home"
-    keymap_global[ "LAlt-E" ] = "End"
-    keymap_global[ "LAlt-F" ] = "Right"
-    keymap_global[ "LAlt-B" ] = "Left"
-    keymap_global[ "LAlt-N" ] = "Down"
-    keymap_global[ "LAlt-P" ] = "Up"
-    keymap_global[ "LAlt-D" ] = "Delete"
-    keymap_global[ "LAlt-H" ] = "Back"
-    keymap_global[ "LAlt-K" ] = "Shift-End", "Delete"
-    keymap_global[ "LAlt" ] = "LCtrl-LAlt" 
+    keymap_global[ "U0-A" ] = "Home"
+    keymap_global[ "U0-E" ] = "End"
+    keymap_global[ "U0-F" ] = "Right"
+    keymap_global[ "U0-B" ] = "Left"
+    keymap_global[ "U0-N" ] = "Down"
+    keymap_global[ "U0-P" ] = "Up"
+    keymap_global[ "U0-D" ] = "Delete"
+    keymap_global[ "U0-H" ] = "Back"
+    keymap_global[ "U0-K" ] = "Shift-End", "Delete"
+
+   
